799bdf952b0fe7e7bd23cfc16db6ddeb6f80c8f2
Ciara Bridget Mary Walsh <CIARA.B.WALSH.2015@mumail.ie>
2017-11-14 10:39:37 +0000

    Update read_data.php to allow browser to access posted values

--- read_data.php
+++ read_data.php
@@ -5,6 +5,7 @@ require '/var/composer/vendor/autoload.php';
 $manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
 $collection = new MongoDB\Collection($manager, 'walsh', 'fyp');
 
+if(!empty($_POST['RSSI'])) {
 	$rssi_value = $_POST['RSSI'];
 	print("<p>" + $_POST['RSSI'] + "</p>");
 	$sensor_value = 1;
@@ -23,6 +24,21 @@ $collection = new MongoDB\Collection($manager, 'walsh', 'fyp');
 		'projection'=> ['RSSI'=>1]];
    	$query = new MongoDB\Driver\Query($filter, $options);
    	$cursor = $manager->executeQuery('walsh.fyp', $query);
+   	print("<p>The contents of the collection walsh.fyp are:</p>");
+   	print_r($cursor->toArray());
+}
+else {
+	$ts = new MongoDB\BSON\UTCDateTime(new DateTime);
+   	$filter = [];
+	//$options=[];
+   	$options = [
+		'batchSize'=>1,
+		'singleBatch'=>true,
+		'projection'=> ['RSSI'=>1],
+		'sort' => ['ts' => 1]];
+   	$query = new MongoDB\Driver\Query($filter, $options);
+   	$cursor = $manager->executeQuery('walsh.fyp', $query);
+   	//$cursor = $collection->find()->sort(array('ts'=> -1))->limit(1);
    	print("<p>The contents of the collection walsh.fyp are:</p>");
    	print_r($cursor->toArray());
 }
