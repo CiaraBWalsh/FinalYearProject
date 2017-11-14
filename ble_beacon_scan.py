584b06c81451cf5dcaefd8938deecaddd9385b42
Ciara Bridget Mary Walsh <CIARA.B.WALSH.2015@mumail.ie>
2017-11-14 10:31:53 +0000

    Added ability to post data to local server

--- ble_beacon_scan.py
+++ ble_beacon_scan.py
@@ -1,5 +1,6 @@
 from bluepy.btle import Scanner, DefaultDelegate
 from statistics import mean
+import requests
 
 rssi_values = []
 BEACON_UUID='4c000215e2c56db5dffb48d2b060d0f5a71096e000010002ba'
@@ -10,8 +11,19 @@ class ScanDelegate(DefaultDelegate):
 
         def handleDiscovery(self, dev, isNewDev, isNewData):
                 if dev.getValueText(255) == BEACON_UUID:
+                    sendData(dev.rssi)
                     print("Beacon found, RSSI = %d dB" % dev.rssi)
                     rssi_values.append(dev.rssi)
+
+
+def sendData(data):
+        try:
+                current_data = {'RSSI':data}
+                r=requests.post('http://localhost/finalyear/read_data.php', data=current_data)
+                print(r.text)
+        except requests.ConnectionError as e:
+                print(e)
+
 
 def main():
         while(True):
