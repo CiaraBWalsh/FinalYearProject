79c48458e67cdefecaf5ab6d93aeea816215c540
Ciara Bridget Mary Walsh <CIARA.B.WALSH.2015@mumail.ie>
2017-11-14 10:38:31 +0000

    Added initial version of MongoDB implementation to read_data.php

--- read_data.php
+++ read_data.php
@@ -1,5 +1,31 @@
 <?php
-	echo($_POST('RSSI'));
+
+require '/var/composer/vendor/autoload.php';
+
+$manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+$collection = new MongoDB\Collection($manager, 'walsh', 'fyp');
+
+	$rssi_value = $_POST['RSSI'];
+	print("<p>" + $_POST['RSSI'] + "</p>");
+	$sensor_value = 1;
+		
+	$ts = new MongoDB\BSON\UTCDateTime(new DateTime);
+   	$data = ['RSSI'=>$rssi_value, 'Sensor'=> $sensor_value, 'time'=>$ts];
+   	try {
+      		$collection->insertOne($data);
+  	} catch (\Exception $e) {
+      		print("<p>Insert failed.</p>");
+   	}
+   	$filter = [];
+   	$options = [
+		'batchSize'=>1,
+		'singleBatch'=>true,
+		'projection'=> ['RSSI'=>1]];
+   	$query = new MongoDB\Driver\Query($filter, $options);
+   	$cursor = $manager->executeQuery('walsh.fyp', $query);
+   	print("<p>The contents of the collection walsh.fyp are:</p>");
+   	print_r($cursor->toArray());
+}
 ?>
 <html>
 	<p> This is hopefully going to work </p>
