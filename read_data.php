9028ea6b0cbe798e08bafbd6985f670c56343aab
Ciara Bridget Mary Walsh <CIARA.B.WALSH.2015@mumail.ie>
2018-03-28 13:55:19 +0100

    clean up: clean up read_data for submission
    Signed-off-by: Ciara Bridget Mary Walsh <CIARA.B.WALSH.2015@mumail.ie>

--- read_data.php
+++ read_data.php
@@ -5,15 +5,20 @@ require '/var/composer/vendor/autoload.php';
 $manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
 $collection = new MongoDB\Collection($manager, 'walsh', 'fyp');
 
+	// Set up MongoDB query to pull only the most recently added document
    	$filter = [];
    	$options = [
 		'batchSize'=>1,
 		'limit'=>1,
 		'sort' => ['time' => (int)-1]];
    	$query = new MongoDB\Driver\Query($filter, $options);
+	
+	//Execute the query
    	$cursor = $manager->executeQuery('walsh.fyp', $query);
+	
 	$rssi = 'RSSI';
 	$sensor = 'Sensor';
+	// Analyse the resulting data and return the status
 	foreach($cursor as $doc) {
 		$obj->rssi = $doc->$rssi;
 		$obj->us = $doc->$sensor;
@@ -21,14 +26,18 @@ $collection = new MongoDB\Collection($manager, 'walsh', 'fyp');
 	}
 
 function dataAnalysis($rssi,$us) {
+	// Linear threshold values
 	$rLimit = -80;
 	$sLimit = 200;
+
+	// Error statuses if device is not found
 	if($rssi == 0) {
 		return (int)-1;
 	}
 	if($us == 0) {
 		return (int)-2;
 	}
+	//Check the data against the thresholds
 	if(($rssi>$rLimit) & ($us<$sLimit)) {
 		return (int)1;
 	}
