203c3a5c9b892c875dded400c269ab9257071395
Ciara Bridget Mary Walsh <CIARA.B.WALSH.2015@mumail.ie>
2018-03-28 12:40:50 +0100

    clean up: clean up startup script for submission
    Signed-off-by: Ciara Bridget Mary Walsh <CIARA.B.WALSH.2015@mumail.ie>

--- startup.sh
+++ startup.sh
@@ -2,16 +2,21 @@
 # startup.sh
 # Start up final year project
 
+# Sleep to ensure system is fully booted before running
 sleep 60
+
+# Turn on Bluetooth
 sudo hciconfig hci0 up
 cd /home/pi/final_year_project
 
+# Create log files
 echo "" > logs/cronlog
 echo "" > logs/blelog
 echo "" > logs/bashlog
 
 exit_code=0
 
+# Run project code
 while true; do
 	sudo python3 ble_beacon_scan.py >> logs/blelog
 	$exit_code = $?
